30bd1e27c56c24a68dcd3a6b460f51427fccc27d
uramer <antonuramer@gmail.com>
2019-03-30 01:37:51 +0100

    force the player (in the majority of cases) to keep the ring equipped

--- main.lua
+++ main.lua
@@ -7,8 +7,12 @@ Arktwend.STARTING_LOCATION = {
     742.98
 }
 Arktwend.CHARGEN_STATUE = "20361-0"
-Arktwend.CHARGEN_RING = "20360-0"
+--Arktwend.CHARGEN_RING = "20360-0"
+Arktwend.CHARGEN_RING = "ringdeskaisers"
 Arktwend.DEATH_TIME = 30
+Arktwend.CHURCH_CELL = "Melee, Monastery Church"
+Arktwend.FURSEAL_CELL = "Melee, Fur Seal Tavern"
+Arktwend.RING_SLOT = 12
 
 
 function Arktwend.OnPlayerEndCharGen(eventStatus, pid)
@@ -33,6 +37,19 @@ function Arktwend.OnServerPostInit(eventStatus)
     --config.playersRespawn = false
 end
 
+function Arktwend.OnPlayerCellChange(eventStatus, pid, prev, curr)
+    --fix players getting spawned into the ceiling
+    if prev == Arktwend.CHURCH_CELL and curr == Arktwend.FURSEAL_CELL then
+        tes3mp.SetPos(pid,
+            6.08,
+            -138.45,
+            -63
+        )
+        tes3mp.SetRot(pid, 0, 0)
+        tes3mp.SendPos(pid)
+    end
+end
+
 
 function Arktwend.sendObjectState(pid, cellDescription, uniqueIndex, state)
     local splitIndex = uniqueIndex:split("-")
@@ -45,21 +62,94 @@ function Arktwend.sendObjectState(pid, cellDescription, uniqueIndex, state)
     tes3mp.SendObjectState(true, false)
 end
 
+function Arktwend.addKaisersRing(pid)
+    tes3mp.ClearInventoryChanges(pid)
+    tes3mp.SetInventoryChangesAction(pid, enumerations.inventory.ADD)
+    tes3mp.AddItemChange(pid, Arktwend.CHARGEN_RING, 1, -1, -1, "")
+    tes3mp.SendInventoryChanges(pid)
+end
+
+function Arktwend.equipKaisersRing(pid)
+    tes3mp.EquipItem(
+        pid,
+        Arktwend.RING_SLOT,
+        Arktwend.CHARGEN_RING,
+        1,
+        -1,
+        -1
+    )
+    tes3mp.SendEquipment(pid)
+end
+
 function Arktwend.OnObjectActivate(eventStatus, pid, cellDescription, objects, players)
     for _, object in pairs(objects) do
         if object.uniqueIndex == Arktwend.CHARGEN_STATUE then
-            local ring = LoadedCells[cellDescription].data.objectData[Arktwend.CHARGEN_RING]
-            if ring ~= nil then
-                if ring.state == false then
-                    ring.state = true
-                    Arktwend.sendObjectState(pid, cellDescription, Arktwend.CHARGEN_RING, true)
-                    tes3mp.MessageBox(pid, -1, "Take the ring and put it on your finger.")
-                end
+            local inventory = Players[pid].data.inventory
+
+            if not inventoryHelper.containsItem(inventory, Arktwend.CHARGEN_RING, -1, -1, "") then
+                inventoryHelper.addItem(inventory, Arktwend.CHARGEN_RING, 1, -1, -1, "")
+                Arktwend.addKaisersRing(pid)
+                tes3mp.MessageBox(pid, -1, "Put the ring on your finger.")
+            else
+                tes3mp.MessageBox(pid, -1, "You already have the ring.")
             end
+
             return customEventHooks.makeEventStatus(false, false)
         end
     end
 end
+
+function Arktwend.OnPlayerInventory(eventStatus, pid)
+    local action = tes3mp.GetInventoryChangesAction(pid)
+    local itemChangesCount = tes3mp.GetInventoryChangesSize(pid)
+
+    if action == enumerations.inventory.REMOVE then
+        for index = 0, itemChangesCount - 1 do
+            local itemRefId = tes3mp.GetInventoryItemRefId(pid, index)
+
+            if itemRefId == Arktwend.CHARGEN_RING then
+                Arktwend.equipKaisersRing(pid)
+                return customEventHooks.makeEventStatus(false, false)
+            end
+        end
+    end
+end
+
+function Arktwend.OnObjectPlace(eventStatus, pid, cellDescription, objects)
+    for _, object in pairs(objects) do
+        if object.refId == Arktwend.CHARGEN_RING then
+            return customEventHooks.makeEventStatus(false, false)
+        end
+    end
+end
+
+function Arktwend.OnContainer(eventStatus, pid, cellDescription, containers)
+    tes3mp.ReadReceivedObjectList()
+    tes3mp.CopyReceivedObjectListToStore()
+
+    local packetOrigin = tes3mp.GetObjectListOrigin()
+    local action = tes3mp.GetObjectListAction()
+
+    if action == enumerations.container.ADD then
+        for containerIndex = 0, tes3mp.GetObjectListSize() - 1 do
+            local uniqueIndex = tes3mp.GetObjectRefNum(containerIndex) .. "-" .. tes3mp.GetObjectMpNum(containerIndex)
+
+            for itemIndex = 0, tes3mp.GetContainerChangesSize(containerIndex) - 1 do
+                local itemRefId = tes3mp.GetContainerItemRefId(containerIndex, itemIndex)
+
+                if itemRefId == Arktwend.CHARGEN_RING then
+                    LoadedCells[cellDescription]:LoadContainers(
+                        pid,
+                        LoadedCells[cellDescription].data.objectData,
+                        {uniqueIndex}
+                    )
+                    return customEventHooks.makeEventStatus(false, false)
+                end
+            end
+        end
+    end
+end
+
 
 ArktwendResurrect = function(pid)
     --respawn the player in the same cell as they died
@@ -76,13 +166,17 @@ function Arktwend.OnPlayerDeath(eventStatus, pid)
             pid
         )
     )
-    return customEventHooks.makeEventStatus(false, true)
+    return customEventHooks.makeEventStatus(false, nil)
 end
 
 customEventHooks.registerHandler("OnPlayerEndCharGen", Arktwend.OnPlayerEndCharGen)
 customEventHooks.registerHandler("OnServerPostInit", Arktwend.OnServerPostInit)
+customEventHooks.registerHandler("OnPlayerCellChange", Arktwend.OnPlayerCellChange)
 
 customEventHooks.registerValidator("OnObjectActivate", Arktwend.OnObjectActivate)
+customEventHooks.registerValidator("OnPlayerInventory", Arktwend.OnPlayerInventory)
+customEventHooks.registerValidator("OnObjectPlace", Arktwend.OnObjectPlace)
+customEventHooks.registerValidator("OnContainer", Arktwend.OnContainer)
 customEventHooks.registerValidator("OnPlayerDeath", Arktwend.OnPlayerDeath)
 
 return Arktwend
